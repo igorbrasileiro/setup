0ab1b14b0c723efcce29eb5d047eaf3bb0a4f7f0
igorbrasileiro <brasileiro456@gmail.com>
2020-11-06 11:01:24 -0300

    add git graph alias

--- git_setup.sh
+++ git_setup.sh
@@ -2,6 +2,7 @@ git config --global user.name igorbrasileiro
 git config --global user.email brasileiro456@gmail.com
 git config --global alias.l1 'log --oneline'
 git config --global alias.sw 'switch'
+git config --global alias.g "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 echo 'CRIANDO ~/.bash_aliases'
 echo "alias gitfep='git fetch && git pull'" >> ~/.bash_aliases
 echo "source ~/.bash_aliases" >> ~/.zshrc
