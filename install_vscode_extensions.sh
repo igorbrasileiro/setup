8c120569d0f6e30c32a15d899b7b5337cb8200f4
igorbrasileiro <brasileiro456@gmail.com>
2021-02-02 17:40:47 -0300

    Add codesandbox teme

--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -6,3 +6,4 @@ code --install-extension esbenp.prettier-vscode
 code --install-extension usernamehw.errorlens
 code --install-extension vscode-icons-team.vscode-icons
 code --install-extension waderyan.gitblame
+code --install-extension ngryman.codesandbox-theme
