01f568ab188e8cd7afb717899627dd4aaae869b9
igorbrasileiro <brasileiro456@gmail.com>
2021-04-01 09:52:52 -0300

    Add bash_aliases and update git_setup.sh

---
+++ .bash_aliases
@@ -0,0 +1,3 @@
+alias gitfep='git fetch && git pull'
+alias flink='yarn link @vtex/gatsby-theme-store @vtex/gatsby-plugin-i18n @vtex/store-ui'
+
--- git_setup.sh
+++ git_setup.sh
@@ -5,8 +5,11 @@ git config --global user.email brasileiro456@gmail.com
 git config --global alias.l1 'log --oneline'
 git config --global alias.sw 'switch'
 git config --global alias.g "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
 echo 'CRIANDO ~/.bash_aliases'
-echo "alias gitfep='git fetch && git pull'" >> ~/.bash_aliases
+cp ./bash_aliases ~/.bash_aliases
+echo 'Adding bash_aliases to ~/.zshrc'
 echo "source ~/.bash_aliases" >> ~/.zshrc
+
 echo "Copying remove_merged_branchs script to home path"
 cp ./remove_merged_branchs.sh ~/remove_merged_branchs.sh
